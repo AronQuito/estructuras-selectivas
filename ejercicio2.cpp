7e64d83e48e9011a3e782ed8734c7862f065e518
AronQuito <acachicatariq@unjbg.edu.pe>
2024-06-19 08:22:08 -0500

    Actualiza ejercicio 1 de estructuras selectivas: Agrega la validacion en entrada 0

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -5,42 +5,46 @@ int main(){
     cout<<"Introduce el ano: "; cin>>a;
     cout<<"Introduce el mes: "; cin>>m;
     cout<<"Introduce el dia: "; cin>>d;
-    switch (m){
-        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-            if(d<=31){
-                cout<<"Fecha valida";
-            }else{
-                cout<<"Fecha no valida";
-            }
-        break;
-        case 4: case 6: case 9: case 11:
-            if (d<=30){
-                cout<<"Fecha valida";
-            }else{
-                cout<<"Fecha no valida";
-            }
-        break;
-        case 2:
-            if((a%4==0 && a%100!=0)||a%400==0){
-                if(d<=29){
+    if(a>0 && m>0 && d>0){
+        switch (m){
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                if(d<=31){
                     cout<<"Fecha valida";
-                }
-                else{
+                }else{
                     cout<<"Fecha no valida";
                 }
-            }
-            else{
-                if(d<=28){
+            break;
+            case 4: case 6: case 9: case 11:
+                if (d<=30){
                     cout<<"Fecha valida";
-                }
-                else{
+                }else{
                     cout<<"Fecha no valida";
                 }
-            }
-        break;
-        default:
-            cout<<"Mes no valido";
-        break;
+            break;
+            case 2:
+                if((a%4==0 && a%100!=0)||a%400==0){
+                    if(d<=29){
+                        cout<<"Fecha valida";
+                    }
+                    else{
+                        cout<<"Fecha no valida";
+                    }
+                }
+                else{
+                    if(d<=28){
+                        cout<<"Fecha valida";
+                    }
+                    else{
+                        cout<<"Fecha no valida";
+                    }
+                }
+            break;
+            default:
+                cout<<"Mes no valido";
+            break;
+        }
+    }else{
+        cout<<"INTRODUCE UN VALOR VALIDO POSITIVO";
     }
     return 0;
 }
