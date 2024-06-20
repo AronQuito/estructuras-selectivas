31b3fe1d8fe58bef45137e10c0f3e0bda8155261
AronQuito <acachicatariq@unjbg.edu.pe>
2024-06-20 18:27:38 -0500

    Agregar ejercicio3 a estructuras selectivas

---
+++ ejercicio3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int a,b,c;
+    double d,x1,x2;
+    cout<<"Introduce los a,b,c: "; cin>>a>>b>>c;
+    d=pow(b,2)-4*a*c;
+    if(d>=0){
+        if(d==0){
+            x1=(pow(d,1/2)-b)/2*a;
+            x2=x1;
+        }
+        else{
+            x1=(pow(d,1/2)-b)/2*a;
+            x2=(-pow(d,1/2)-b)/2*a;
+        }
+        cout<<"x1: "<<x1;
+        cout<<"x2: "<<x2;
+    }
+    else{
+        cout<<"No hay raices en Reales";
+    }
+    return 0;
+}
